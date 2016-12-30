e8d23decbc66d0e5daa680fc2b3785ddf0633a84
Daniel Shaw <dls1992@live.co.uk>
2016-12-30 14:17:42 +0000

    Recreation of Repository

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Paddle initialised");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//set variable for current position
+		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
+
+		//get mouse position
+		float mousePos = (Input.mousePosition.x / Screen.width * 16);
+
+		//set new mouse position
+
+		paddlePos.x = Mathf.Clamp (mousePos, 0f, 16f);
+
+		//change paddle position to match
+		this.transform.position=paddlePos;
+
+	}
+}
